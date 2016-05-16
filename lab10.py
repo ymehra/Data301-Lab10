c745da2bbf28991fcd38ba8ba86abae368c653df
Yash Mehra <ymehra@calpoly.edu>
2016-05-16 11:43:52 -0700

    Home file for entire lab

--- lab10.py
+++ lab10.py
@@ -1,3 +1,15 @@
 #below are imports
 import numpy as np
 import re
+
+# import ratings
+
+
+# Collaborative predictions
+
+# Item-based predictions
+
+# Nearest Neighbor Collaborative predictions
+
+# Nearest Neighbor Item-based predictions
+
