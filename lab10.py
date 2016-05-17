b4efbce33bc6ee42ebe51276d60a1aeb773133c1
shsu03 <shsu03@calpoly.edu>
2016-05-17 15:26:10 -0700

    adding ratings import

--- lab10.py
+++ lab10.py
@@ -1,8 +1,48 @@
 #below are imports
 import numpy as np
 import re
+import math
 
 # import ratings
+
+ratingsFile = "/home/dekhtyar/data/jester-data-1.csv"
+
+## some useful constants
+
+NUM_USERS = 24983   ## number of users in the dataset
+NUM_JOKES = 100     ## number of jokes in the dataset
+
+EPSILON = 0.0001    ## useful for assertions over floats
+
+rawRatingsTable = []  ## use rawRatings variable to store the NumPy array of
+                 ## ratings from the data file
+userActivity = []  ## put the first column of the rawRatingsTable array here
+rawRatings = []  ## put the rest of the rawRatingsTable array here
+
+### place the code loading the array of ratings below
+def load_ratings():
+    with open(ratingsFile, 'r') as file:
+        rows = []
+
+        for line in file:
+            ratings = line.split(",")
+            rows.append(np.asarray(ratings, dtype = 'float'))
+
+        rawRatingsTable = np.asarray(rows)
+
+        # SPLIT RAW RATINGS MATRIX
+        userActivityList = []
+        rawRatingsList = []
+        for row in range(rawRatingsTable.shape[0]):
+            userActivityList.append(rawRatingsTable[row, 0])
+            rawRatingsList.append(rawRatingsTable[row, 1:])
+
+        userActivity = np.asarray(userActivityList)
+        rawRatings = np.asarray(rawRatingsList)
+
+
+
+def mean_utility(person, jokeId):
 
 
 # Collaborative predictions
