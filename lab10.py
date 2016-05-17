Fixed Bugs and updated the average calculator

lab10.py
@@ -14,8 +14,6 @@ EPSILON = 0.0001    ## useful for assertions over floats
 
 rawRatingsTable = []  ## use rawRatings variable to store the NumPy array of
                  ## ratings from the data file
-userActivity = []  ## put the first column of the rawRatingsTable array here
-rawRatings = []  ## put the rest of the rawRatingsTable array here
 
 ### loading the array of ratings
 def load_ratings():
@@ -27,26 +25,28 @@ def load_ratings():
             rows.append(np.asarray(ratings, dtype = 'float'))
 
         rawRatingsTable = np.asarray(rows)
-
         # SPLIT RAW RATINGS MATRIX
         userActivityList = []
         rawRatingsList = []
         for row in range(rawRatingsTable.shape[0]):
             userActivityList.append(rawRatingsTable[row, 0])
             rawRatingsList.append(rawRatingsTable[row, 1:])
-
         userActivity = np.asarray(userActivityList)
         rawRatings = np.asarray(rawRatingsList)
 
+        return userActivity, rawRatings
 
 # Collaborative predictions
 
 # mean utility of a joke for all users who rated it
 def coll_average(person, jokeId):
     # get all ratings for jokeId
-    ratings = rawRatings[:][jokeId-1]
+    ratings = []
+    for row in range(rawRatings.shape[0]):
+        ratings.append(rawRatings[row, jokeId-1])
     count = 0
-
+    rSum = 0
+    ratings  = np.asarray(ratings)
     for i in range(ratings.shape[0]):
         # if valid rating and not person in question's rating
         if ratings[i] != 99 and i != person+1:
@@ -73,3 +73,6 @@ def item_average(person, jokeId):
 # Nearest Neighbor Collaborative predictions
 
 # Nearest Neighbor Item-based predictions
+
+userActivity, rawRatings = load_ratings()
+print (coll_average(1, 1))